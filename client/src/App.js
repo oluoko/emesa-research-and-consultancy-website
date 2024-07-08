46a59bd12cdb5500788d96cd40d5c231df90b739
Brian Otieno <brian@briaN.briaN>
2024-07-08 13:35:06 -0400

    Emesa Research and Consultancy

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar.jsx";
+import Home from "./Components/Tabs/Home/Home.jsx";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact component={Home} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/Components/Tabs/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section id="home">
+      <h1>Welcome to My Website</h1>
+    </section>
+  );
+};
+
+export default Home;
