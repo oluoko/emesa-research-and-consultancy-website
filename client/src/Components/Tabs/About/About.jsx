e49939d27924a3c8e9656e2ced13d45411509cb6
Brian Otieno <brian@briaN.briaN>
2024-07-09 15:15:03 -0400

    Emesa Research and Consultancy

--- client/src/Components/Tabs/About/About.jsx
+++ client/src/Components/Tabs/About/About.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
 import "./About.css";
 
 const About = () => {
+  const [images, setImages] = useState([]);
+  const galleryRef = useRef(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    // Import all images from the folder
+    const importAll = (r) => r.keys().map(r);
+    const images = importAll(
+      require.context(
+        "../../../Assets/Images/Staff",
+        false,
+        /\.(png|jpe?g|svg)$/
+      )
+    );
+    setImages(images);
+  }, []);
+
+  useEffect(() => {
+    if (location.pathname === "/about" && galleryRef.current) {
+      const scrollToBottomAndTop = () => {
+        galleryRef.current.scrollTop = galleryRef.current.scrollHeight;
+        setTimeout(() => {
+          galleryRef.current.scrollTop = 0;
+        }, 2000);
+      };
+      scrollToBottomAndTop();
+    }
+  }, [location]);
+
   return (
     <div className="screen-container">
       <div id="about" className="screen section-container">
         <section className="left">
-          <h3>About Emesa Research</h3>
+          <h2>About Emesa Research and Consultancy</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ab
             quibusdam sint omnis eius ea distinctio nesciunt facilis nostrum
@@ -20,8 +50,13 @@ const About = () => {
           </p>
         </section>
         <section className="right">
-          <div className="gallery">
-            <p>this will contain the organization's gallery</p>
+          <h3>Here are some of our staff members.</h3>
+          <div className="gallery-container" ref={galleryRef}>
+            {images.map((image, index) => (
+              <div key={index} className="gallery-item">
+                <img src={image} alt={`Image ${index + 1}`} />
+              </div>
+            ))}
           </div>
         </section>
       </div>
