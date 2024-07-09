9d756a196feac4d49173f04f4a42ed371fbe2514
Brian Otieno <brian@briaN.briaN>
2024-07-09 14:44:30 -0400

    Emesa Research and Consultancy

--- client/src/Components/Tabs/Home/Home.jsx
+++ client/src/Components/Tabs/Home/Home.jsx
@@ -1,15 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ImageGallery from "react-image-gallery";
+import { useDropzone } from "react-dropzone";
+import "react-image-gallery/styles/css/image-gallery.css";
+
 import "./Home.css";
 import Ads from "../../Ads/Ads.jsx";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [images, setImages] = useState([]);
+  const [uploadedImages, setUploadedImages] = useState([]);
+
+  useEffect(() => {
+    // Import all images from the folder
+    const importAll = (r) => r.keys().map(r);
+    const images = importAll(
+      require.context(
+        "../../../Assets/Images/Staff",
+        false,
+        /\.(png|jpe?g|svg)$/
+      )
+    );
+    setImages(
+      images.map((image, index) => ({
+        original: image,
+        thumbnail: image,
+        originalAlt: `Image ${index + 1}`,
+        thumbnailAlt: `Thumbnail ${index + 1}`,
+      }))
+    );
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/*",
+    onDrop: (acceptedFiles) => {
+      const newImages = acceptedFiles.map((file) => ({
+        original: URL.createObjectURL(file),
+        thumbnail: URL.createObjectURL(file),
+        originalAlt: file.name,
+        thumbnailAlt: file.name,
+      }));
+      setUploadedImages([...uploadedImages, ...newImages]);
+    },
+  });
+
+  const combinedImages = [...images, ...uploadedImages];
+
   return (
     <div className="screen-container">
       <div id="home" className="screen section-container">
         <section className="left">
-          <div className="image-shuffler">
-            image shuffler will be contained here
+          <div className="carousel-container">
+            <ImageGallery
+              items={combinedImages}
+              infinite
+              autoPlay
+              showThumbnails
+            />
           </div>
         </section>
         <section className="right">
