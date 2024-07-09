44a3feab1d7cb085f349d313e164aaeed26a5407
Brian Otieno <brian@briaN.briaN>
2024-07-09 03:11:33 -0400

    Emesa Research and Consultancy

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import Home from "./Components/Tabs/Home/Home.jsx";
+import Ads from "./Components/Ads/Ads.jsx";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Home />
+      <Ads />
       <Routes>
         <Route path="/" element={Home} />
       </Routes>
