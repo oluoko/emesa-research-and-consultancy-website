a76c87f44bd82a25c4b3a4fdcaa4931682bf13dd
Brian Otieno <brian@briaN.briaN>
2024-07-31 08:30:28 -0400

    Emesa Research and Consultancy

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import BlogScreen from "./Components/Screens/BlogScreen/BlogScreen.tsx";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BlogScreen />
+    </div>
+  );
 }
 
 export default App;
