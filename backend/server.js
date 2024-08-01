2bf1bc0ee2861c414f5a4f4769e83a16cc4d2dd4
oluoko <brianotieno586@gmail.com>
2024-08-01 05:27:20 -0400

    Emesa Research and Consultancy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,45 @@
-const colors = require("colors");
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import colors from "colors";
+import postRoutes from "./routes/posts.js";
 
-console.log("Backend server is running sir!".yellow.bold.underline.inverse);
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(
+  express.urlencoded({
+    limit: "30mb",
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json({
+    author: "Ogulcan",
+    message: "Hello, MERN is awesome!",
+  });
+});
+
+app.use("/posts", postRoutes);
+
+const PORT = process.env.PORT || 8080;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(
+        `Server is running on port: ${PORT}, sir...`.yellow.bold.inverse
+      );
+    });
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
