924e7cb8bcd0eeb190f5a7bb54f77c0f21af55c5
oluoko <brianotieno586@gmail.com>
2024-08-07 13:31:09 -0400

    Emesa Research and Consultancy

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,6 +9,9 @@ const {
   deleteUser,
   getUserById,
   updateUser,
+  updateBloggerApplicationStatus,
+  updateEmployeeApplicationStatus,
+  updateAttacheeApplicationStatus,
 } = require("../controllers/userController.js");
 const { protect, admin } = require("../middleware/authMiddleware.js");
 
@@ -26,5 +29,24 @@ router
   .delete(protect, admin, deleteUser)
   .get(protect, admin, getUserById)
   .put(protect, admin, updateUser);
+
+router.put(
+  "/:id/blogger-application",
+  protect,
+  admin,
+  updateBloggerApplicationStatus
+);
+router.put(
+  "/:id/employee-application",
+  protect,
+  admin,
+  updateEmployeeApplicationStatus
+);
+router.put(
+  "/:id/attachee-application",
+  protect,
+  admin,
+  updateAttacheeApplicationStatus
+);
 
 module.exports = router;
