bd1287b88044ec51edb64dcb4b6ffd57eb58aa61
oluoko <brianotieno586@gmail.com>
2024-08-09 07:50:13 -0400

    Emesa Research and Consultancy: server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,8 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-
+app.use(cors());
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
