b4df8028c891008730b5c64683f3544617694517
oluoko <brianotieno586@gmail.com>
2024-08-11 01:50:24 -0400

    Emesa Research and Consultancy

--- client/src/Components/Screens/RegisterScreen/RegisterScreen.jsx
+++ client/src/Components/Screens/RegisterScreen/RegisterScreen.jsx
@@ -7,13 +7,13 @@ const RegisterScreen = () => {
       className="
 "
     >
-      <div className="contacts-form form max-w-md mx-auto my-10 p-10 border rounded-lg shadow-lg bg-black flex flex-col items-center  text-white ">
-        <h className="py-3">REGISTER AN ACCOUNT</h>
-        <form>
+      <div className="contacts-form form max-w-md mx-auto my-5 p-10 border rounded-lg shadow-lg bg-black flex flex-col items-center  text-white ">
+        <h className="mb-8 text-2xl text-orange-500">REGISTER AN ACCOUNT</h>
+        <form className=" flex flex-col items-center ">
           <input
             type="text"
             id="name"
-            className="  font-bold mb-4 text-xl shadow appearance-none border rounded w-full py-3 px-3 text-gray-600 bg-inherit leading-tight focus:outline-none focus:shadow-outline"
+            className="  font-bold mb-4 text-xl shadow appearance-none border rounded w-full py-3 px-3 text-gray-600 bg-inherit leading-tight focus;border-orange-500 focus:outline-none focus:shadow-outline"
             placeholder="Your Name"
           />
 
@@ -34,14 +34,14 @@ const RegisterScreen = () => {
           <input
             type="password"
             id="password"
-            className="shadow appearance-none border rounded w-full  text-white text-xl text-sm font-bold mb-2 py-2 px-3 text-gray-600 bg-inherit leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full  font-bold mb-4 py-3 px-3 text-gray-600 bg-inherit leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Password"
           />
 
           <input
             type="password"
             id="confirmPassword"
-            className="shadow appearance-none border rounded w-full  text-white text-xl text-sm font-bold mb-2 py-2 px-3 text-gray-600 bg-inherit leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full font-bold mb-4 py-3 px-3 text-gray-600 bg-inherit leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Confirm Your Password"
           />
 
@@ -49,20 +49,26 @@ const RegisterScreen = () => {
             <label className="inline-flex items-center">
               <input type="checkbox" className="form-checkbox" />
               <span className="ml-2 text-white">
-                Agree with statments in <a href="">Terms of Service</a>
+                Agree with statments in{" "}
+                <a className="text-blue-500 hover:text-orange-500" href="">
+                  Terms of Service
+                </a>
               </span>
             </label>
           </div>
 
           <button
             type="submit"
-            className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+            className=" font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
           >
             Register
           </button>
 
           <p className="mt-5">
-            Already have and account?<Link to="/login">Log in</Link>
+            Already have and account?{" "}
+            <Link className="text-blue-500 hover:text-orange-500" to="/login">
+              Log in
+            </Link>
           </p>
         </form>
       </div>
