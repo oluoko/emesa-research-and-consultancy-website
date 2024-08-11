19ec43454e47b1d2066160b4a8fd11a16b341fa5
oluoko <brianotieno586@gmail.com>
2024-08-11 01:52:44 -0400

    Emesa Research and Consultancy

--- client/src/Components/Screens/LoginScreen/LoginScreen.jsx
+++ client/src/Components/Screens/LoginScreen/LoginScreen.jsx
@@ -8,83 +8,73 @@ const LoginScreen = () => {
       className="
 "
     >
-      <div className="contacts-form form max-w-md mx-auto my-10 p-5 border rounded-lg shadow-lg bg-black ">
-        <h3>Contacts Form</h3>
-        <form>
-          <div className="flex justify-around">
-            <div className="m-2">
-              <label
-                className="block text-white text-sm font-bold mb-2 text-xl"
-                htmlFor="name"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-600 bg-inherit leading-tight focus:outline-none focus:shadow-outline"
-                placeholder="John Doe"
-              />
-            </div>
-            <div className="m-2">
-              <label
-                className="block text-white text-xl text-sm font-bold mb-2"
-                htmlFor="email"
-              >
-                E-Mail
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-600 bg-inherit leading-tight focus:outline-none focus:shadow-outline"
-                placeholder="j.doe@company.com"
-              />
-            </div>
-          </div>
+      <div className="contacts-form form max-w-md mx-auto my-5 p-10 border rounded-lg shadow-lg bg-black flex flex-col  text-white ">
+        <h className="mb-8 text-2xl text-orange-500">SIGN IN</h>
+        <p>Enter your email and password to sign in</p>
+        <form className=" flex flex-col items-center ">
+          <input
+            type="text"
+            id="name"
+            className="  font-bold mb-4 text-xl shadow appearance-none border rounded w-full py-3 px-3 text-gray-600 bg-inherit leading-tight  focus:outline-none focus:shadow-outline"
+            placeholder="Your Name"
+          />
+
+          <input
+            type="email"
+            id="email"
+            className="shadow appearance-none border rounded w-full   font-bold mb-4 py-3 px-3 text-gray-600 bg-inherit leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Your Email"
+          />
+
+          <input
+            type="email"
+            id="confirmEmail"
+            className="shadow appearance-none border rounded w-full   font-bold mb-4 py-3 px-3 text-gray-600 bg-inherit leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Confirm Your Email"
+          />
+
+          <input
+            type="password"
+            id="password"
+            className="shadow appearance-none border rounded w-full  font-bold mb-4 py-3 px-3 text-gray-600 bg-inherit leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Password"
+          />
+
+          <input
+            type="password"
+            id="confirmPassword"
+            className="shadow appearance-none border rounded w-full font-bold mb-4 py-3 px-3 text-gray-600 bg-inherit leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Confirm Your Password"
+          />
 
           <div className="m-2">
-            <label
-              className="block text-white text-xl font-bold mb-2"
-              htmlFor="description"
-            >
-              What is this about?
-            </label>
-            <textarea
-              id="description"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-600 bg-inherit leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Hello there,
-
-There is a bug in... How can I apply to be an employee... At what time are your offices open..."
-              rows="5"
-            ></textarea>
-          </div>
-          <div className="m-2">
-            {/* <label className="inline-flex items-center">
-            <input type="checkbox" className="form-checkbox" />
-            <span className="ml-2">
-              By submitting this form, you agree to the Privacy Policy
-            </span>
-          </label> */}
-          </div>
-          <div className="m-2 flex justify-between">
-            <button
-              type="submit"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-              Submit
-            </button>
-            <div className="text-center mt-4 text-white flex flex-col justify-center items-center">
-              <p>Hate Contact forms?</p>
-              <p>
-                <a
-                  href="mailto:researchemesa@gmail.com"
-                  className="text-sm text-blue-500 hover:underline"
-                >
-                  Our email
+            <label className="inline-flex items-center">
+              <input type="checkbox" className="form-checkbox" />
+              <span className="ml-2 text-white">
+                Agree with statments in{" "}
+                <a className="text-blue-500 hover:text-orange-500" href="">
+                  Terms of Service
                 </a>
-              </p>
-            </div>
+              </span>
+            </label>
           </div>
+
+          <button
+            type="submit"
+            className=" font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+          >
+            Register
+          </button>
+
+          <p className="mt-5">
+            Don&quot;t have an account?{" "}
+            <Link
+              className="text-blue-500 hover:text-orange-500"
+              to="/register"
+            >
+              Sign up
+            </Link>
+          </p>
         </form>
       </div>
     </div>
