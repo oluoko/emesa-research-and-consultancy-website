163736713355f9805e40bec83d32428eaad1eb0e
oluoko <brianotieno586@gmail.com>
2024-08-11 04:25:05 -0400

    Emesa Research and Consultancy: AttacheeApplicationScreen.jsx

--- client/src/Components/Screens/AttacheeApplication/AttacheeApplicationScreen.jsx
+++ client/src/Components/Screens/AttacheeApplication/AttacheeApplicationScreen.jsx
@@ -3,7 +3,12 @@ import { Link } from "react-router-dom";
 
 const AttacheeApplicationScreen = () => {
   return (
-    <div className="flex flex-col  vh-100 w-full items-center justify-center">
+    <div className=" relative flex flex-col  vh-100  items-center justify-center m-2">
+      <button className="fixed top-4 left-4">
+        <Link to="/careers" className="text-2xl ">
+          Back
+        </Link>
+      </button>
       <h className="mt-6 text-2xl font-extrabold text-orange-500">
         APPLY FOR ATTACHMENT
       </h>
@@ -55,13 +60,20 @@ const AttacheeApplicationScreen = () => {
             <label htmlFor="message" className="">
               UPLOAD CV
             </label>
-            <textarea
-              type="message"
-              id="message"
-              className="col-md-9 pe-5 shadow appearance-none border rounded font-bold m-4 mb-6 py-4 px-3 text-gray-600 bg-inherit leading-tight focus:outline-none focus:shadow-outline focus:border-orange-500 w-full"
-              placeholder="Message to the employee telling us about yourself..."
-            />
+            <div className="   w-full m-4 mb-2 py-4 px-3">
+              <input
+                className="form-control form-control-lg border rounded font-bold  text-gray-600 bg-inherit leading-tight focus:outline-none focus:shadow-outline focus:border-orange-500"
+                id="formFileLg"
+                type="file"
+              />
+              <div className="small text-muted mt-2">
+                Upload your CV/Resume or any other relevant file. Max file size
+                10 MB
+              </div>
+            </div>
           </div>
+
+          <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700 w-full" />
 
           <button
             type="submit"
