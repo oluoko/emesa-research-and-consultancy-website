a84d7def22d91431fede1492e795bf41b98ac54d
oluoko <brianotieno586@gmail.com>
2024-08-11 04:11:20 -0400

    Emesa Research and Consultancy: AttacheeApplicationScreen.jsx

--- client/src/Components/Screens/AttacheeApplication/AttacheeApplicationScreen.jsx
+++ client/src/Components/Screens/AttacheeApplication/AttacheeApplicationScreen.jsx
@@ -49,6 +49,20 @@ const AttacheeApplicationScreen = () => {
             />
           </div>
 
+          <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700 w-full" />
+
+          <div className="flex justify-between items-center  w-full my-2">
+            <label htmlFor="message" className="">
+              UPLOAD CV
+            </label>
+            <textarea
+              type="message"
+              id="message"
+              className="col-md-9 pe-5 shadow appearance-none border rounded font-bold m-4 mb-6 py-4 px-3 text-gray-600 bg-inherit leading-tight focus:outline-none focus:shadow-outline focus:border-orange-500 w-full"
+              placeholder="Message to the employee telling us about yourself..."
+            />
+          </div>
+
           <button
             type="submit"
             className=" font-bold py-2 px-4 rounded text-xl focus:outline-none focus:shadow-outline w-full"
