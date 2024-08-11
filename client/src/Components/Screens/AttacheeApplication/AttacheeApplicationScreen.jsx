c172f3d200b326fd84714139368b97993bce40f2
oluoko <brianotieno586@gmail.com>
2024-08-11 01:03:37 -0400

    Emesa Research and Consultancy

--- client/src/Components/Screens/AttacheeApplication/AttacheeApplicationScreen.jsx
+++ client/src/Components/Screens/AttacheeApplication/AttacheeApplicationScreen.jsx
@@ -1,91 +1,95 @@
-import React from 'react'
+import React from "react";
 
 const AttacheeApplicationScreen = () => {
   return (
-    <section class="vh-100" style="background-color: #2779e2;">
-  <div class="container h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-xl-9">
+    <section className="vh-100" style="background-color: #2779e2;">
+      <div className="container h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-xl-9">
+            <h1 className="text-white mb-4">Apply for a job</h1>
 
-        <h1 class="text-white mb-4">Apply for a job</h1>
+            <div className="card" style="border-radius: 15px;">
+              <div className="card-body">
+                <div className="row align-items-center pt-4 pb-3">
+                  <div className="col-md-3 ps-5">
+                    <h6 className="mb-0">Full name</h6>
+                  </div>
+                  <div className="col-md-9 pe-5">
+                    <input
+                      type="text"
+                      className="form-control form-control-lg"
+                    />
+                  </div>
+                </div>
 
-        <div class="card" style="border-radius: 15px;">
-          <div class="card-body">
+                <hr className="mx-n3" />
 
-            <div class="row align-items-center pt-4 pb-3">
-              <div class="col-md-3 ps-5">
+                <div className="row align-items-center py-3">
+                  <div className="col-md-3 ps-5">
+                    <h6 className="mb-0">Email address</h6>
+                  </div>
+                  <div className="col-md-9 pe-5">
+                    <input
+                      type="email"
+                      className="form-control form-control-lg"
+                      placeholder="example@example.com"
+                    />
+                  </div>
+                </div>
 
-                <h6 class="mb-0">Full name</h6>
+                <hr className="mx-n3" />
 
-              </div>
-              <div class="col-md-9 pe-5">
+                <div className="row align-items-center py-3">
+                  <div className="col-md-3 ps-5">
+                    <h6 className="mb-0">Full name</h6>
+                  </div>
+                  <div className="col-md-9 pe-5">
+                    <textarea
+                      className="form-control"
+                      rows="3"
+                      placeholder="Message sent to the employer"
+                    ></textarea>
+                  </div>
+                </div>
 
-                <input type="text" class="form-control form-control-lg" />
+                <hr className="mx-n3" />
 
+                <div className="row align-items-center py-3">
+                  <div className="col-md-3 ps-5">
+                    <h6 className="mb-0">Upload CV</h6>
+                  </div>
+                  <div className="col-md-9 pe-5">
+                    <input
+                      className="form-control form-control-lg"
+                      id="formFileLg"
+                      type="file"
+                    />
+                    <div className="small text-muted mt-2">
+                      Upload your CV/Resume or any other relevant file. Max file
+                      size 50 MB
+                    </div>
+                  </div>
+                </div>
+
+                <hr className="mx-n3" />
+
+                <div className="px-5 py-4">
+                  <button
+                    type="submit"
+                    data-mdb-button-init
+                    data-mdb-ripple-init
+                    className="btn btn-primary btn-lg"
+                  >
+                    Send application
+                  </button>
+                </div>
               </div>
             </div>
-
-            <hr class="mx-n3">
-
-            <div class="row align-items-center py-3">
-              <div class="col-md-3 ps-5">
-
-                <h6 class="mb-0">Email address</h6>
-
-              </div>
-              <div class="col-md-9 pe-5">
-
-                <input type="email" class="form-control form-control-lg" placeholder="example@example.com" />
-
-              </div>
-            </div>
-
-            <hr class="mx-n3">
-
-            <div class="row align-items-center py-3">
-              <div class="col-md-3 ps-5">
-
-                <h6 class="mb-0">Full name</h6>
-
-              </div>
-              <div class="col-md-9 pe-5">
-
-                <textarea class="form-control" rows="3" placeholder="Message sent to the employer"></textarea>
-
-              </div>
-            </div>
-
-            <hr class="mx-n3">
-
-            <div class="row align-items-center py-3">
-              <div class="col-md-3 ps-5">
-
-                <h6 class="mb-0">Upload CV</h6>
-
-              </div>
-              <div class="col-md-9 pe-5">
-
-                <input class="form-control form-control-lg" id="formFileLg" type="file" />
-                <div class="small text-muted mt-2">Upload your CV/Resume or any other relevant file. Max file
-                  size 50 MB</div>
-
-              </div>
-            </div>
-
-            <hr class="mx-n3">
-
-            <div class="px-5 py-4">
-              <button type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg">Send application</button>
-            </div>
-
           </div>
         </div>
-
       </div>
-    </div>
-  </div>
-</section>
-  )
-}
+    </section>
+  );
+};
 
-export default AttacheeApplicationScreen
+export default AttacheeApplicationScreen;
