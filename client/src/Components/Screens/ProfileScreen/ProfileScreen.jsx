f18d9547495d46cac671839e5710511eada7500d
oluoko <brianotieno586@gmail.com>
2024-08-11 04:44:11 -0400

    Emesa Research and Consultancy: ProfileScreen.jsx

--- client/src/Components/Screens/ProfileScreen/ProfileScreen.jsx
+++ client/src/Components/Screens/ProfileScreen/ProfileScreen.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ProfileScreen = () => {
-  return <div>ProfileScreen</div>;
+  return (
+    <div className="relative">
+      <button className="button fixed top-4 left-4 font-bold py-2 px-4 rounded text-xl focus:outline-none focus:shadow-outline">
+        <Link to="/careers" className="text-2xl ">
+          Back
+        </Link>
+      </button>
+    </div>
+  );
 };
 
 export default ProfileScreen;
