3cb28d383a12648430e6d805e5867e7d20be5456
oluoko <brianotieno586@gmail.com>
2024-08-11 03:18:24 -0400

    Emesa Research and Consultancy

--- client/src/Components/Screens/AttacheeApplication/AttacheeApplicationScreen.jsx
+++ client/src/Components/Screens/AttacheeApplication/AttacheeApplicationScreen.jsx
@@ -7,7 +7,7 @@ const AttacheeApplicationScreen = () => {
       <h className="m-6 text-2xl text-orange-500">APPLY FOR ATTACHMENT</h>
       <div className="contacts-form form max-w-md mx-auto my-5 p-10 border rounded-lg shadow-lg bg-gray-800 flex flex-col   text-white w-full">
         <form className=" flex flex-col items-center  ">
-          <div className="row align-items-center pt-4 pb-3 border-y-gray-400">
+          <div className="row align-items-center pt-4 pb-3  border-y-2 border-y-gray-400 w-full">
             <label htmlFor="name" className="col-md-3 ps-5">
               FULL NAME
             </label>
@@ -23,18 +23,8 @@ const AttacheeApplicationScreen = () => {
             type="submit"
             className=" font-bold py-2 px-4 rounded text-xl focus:outline-none focus:shadow-outline w-full"
           >
-            Sign In
+            SEND APPLICATION
           </button>
-
-          <p className="mt-5 text-xl">
-            Don&quot;t have an account?{" "}
-            <Link
-              className="text-blue-500 hover:text-orange-500"
-              to="/register"
-            >
-              SEND APPLICATION
-            </Link>
-          </p>
         </form>
       </div>
     </div>
