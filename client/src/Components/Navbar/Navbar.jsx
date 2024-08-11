67bb911f5190dad6641c9e7cab60e93c6e6b73f8
oluoko <brianotieno586@gmail.com>
2024-08-11 14:29:25 -0400

    Emesa Research and Consultancy

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import userProfile from "../../Assets/userProfile.png";
 import logo from "../../Assets/Logos/logo.png";
 import { Link } from "react-router-dom";
+// import { LiaTimesSolid } from "react-icons/lia";
+// import { IoMenu } from "react-icons/io5";
 
 const Navbar = ({ screen }) => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [navbarOpen, setNavbaropen] = useState(false);
+
   return (
     <div id="nav-container">
       <nav>
@@ -113,13 +118,30 @@ const Navbar = ({ screen }) => {
                 </div>
               </li>
               <li>
-                <Link to="/profile">
-                  <img
-                    src={userProfile}
-                    alt="Profile"
-                    className="profile-pic w-9 h-9"
-                  />
-                </Link>
+                <img
+                  src={userProfile}
+                  alt="Profile"
+                  className="profile-pic w-9 h-9"
+                  onClick={() => setShowMenu(!showMenu)}
+                />
+                {showMenu && (
+                  <div className="dropdown-menu flex flex-col justify-center items-center fixed top-20 right-40  text-xl bg-slate-200 w-48 p-4 rounded-2xl shadow shadow-slate-700">
+                    <Link
+                      to="/profile"
+                      className="w-full h-full p-3 hover:rounded-t-xl hover:bg-slate-500 flex flex-col justify-center items-center hover:text-slate-200"
+                    >
+                      Profile
+                    </Link>
+                    <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700 w-full " />
+
+                    <button
+                      className="logout-btn p-3 w-full h-full hover:rounded-b-xl hover:bg-slate-500 hover:text-slate-200
+                    "
+                    >
+                      Logout
+                    </button>
+                  </div>
+                )}
               </li>
             </>
           )}
@@ -150,9 +172,30 @@ const Navbar = ({ screen }) => {
                 </Link>
               </li>
               <li>
-                <Link to="/profile" className="user-profile-link">
-                  <img src={userProfile} alt="Profile" className="w-9 h-9" />
-                </Link>
+                <img
+                  src={userProfile}
+                  alt="Profile"
+                  className="profile-pic w-9 h-9"
+                  onClick={() => setShowMenu(!showMenu)}
+                />
+                {showMenu && (
+                  <div className="dropdown-menu flex flex-col justify-center items-center fixed top-20 right-40  text-xl bg-slate-200 w-48 p-4 rounded-2xl shadow shadow-slate-700">
+                    <Link
+                      to="/profile"
+                      className="w-full h-full p-3 hover:rounded-t-xl hover:bg-slate-500 flex flex-col justify-center items-center hover:text-slate-200"
+                    >
+                      Profile
+                    </Link>
+                    <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700 w-full " />
+
+                    <button
+                      className="logout-btn p-3 w-full h-full hover:rounded-b-xl hover:bg-slate-500 hover:text-slate-200
+                    "
+                    >
+                      Logout
+                    </button>
+                  </div>
+                )}
               </li>
             </>
           )}
