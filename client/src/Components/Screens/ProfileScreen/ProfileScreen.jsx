ecc3f6a46dbe5ab0061508e258912c756cb91ce8
oluoko <brianotieno586@gmail.com>
2024-08-11 06:55:01 -0400

    Emesa Research and Consultancy

--- client/src/Components/Screens/ProfileScreen/ProfileScreen.jsx
+++ client/src/Components/Screens/ProfileScreen/ProfileScreen.jsx
@@ -1,28 +1,36 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import profileImage from "../../../Assets/userProfile.png";
 
 const ProfileScreen = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    // Save the current location's pathname in localStorage or a state
+    localStorage.setItem("lastPage", location.pathname);
+  }, [location]);
   return (
-    <div className="relative flex flex-col justify-center items-center ">
-      <button className="button fixed top-4 left-4 font-bold py-2 px-4 rounded text-xl focus:outline-none focus:shadow-outline">
-        <Link to="/careers" className="text-2xl ">
-          Back
-        </Link>
+    <div className="relative flex flex-col justify-center items-center">
+      <button
+        onClick={() => navigate(-1)}
+        className="button fixed top-4 left-4 font-bold text-2xl py-2 px-4 rounded text-xl focus:outline-none focus:shadow-outline"
+      >
+        Back
       </button>
-      <div className="w-10/12 mx-8 flex justify-around  h-4/5 mt-28 gap-4 ">
-        <div className="w-1/4  p-4 bg-slate-300 rounded-xl">
+      <div className="w-11/12 p-4  h-full mx-8 flex justify-around  h-3/6 mt-28 gap-4  shadow-lg border-2 rounded-xl  bg-gray-500 ">
+        <div className="w-1/4  p-4 rounded-xl">
           <img
             src={profileImage}
-            className="w-full rounded-full border-8 hover:border-orange-500"
+            className="w-full rounded-full hover:border-2 hover:border-orange-500 "
           />
         </div>
 
-        <div className=" p-3 w-3/4  flex flex-col justify-center items-center bg-black rounded-xl">
-          <div className="text-white text-2xl">
+        <div className=" p-3 w-3/4  flex flex-col justify-center items-center  rounded-xl bg-white ">
+          <div className="text-gray-400 text-2xl">
             <div className="grid md:grid-cols-2 text-sm">
               <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">First Name</div>
+                <div className="px-4 py-2 font-semibold text-black">NAME: </div>
                 <div className="px-4 py-2">Jane</div>
               </div>
               <div className="grid grid-cols-2">
