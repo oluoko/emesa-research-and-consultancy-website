94a98dde58e1e5ee256585e44ad98dbbd9bd7eb2
oluoko <brianotieno586@gmail.com>
2024-08-13 19:45:16 +0300

    Emesa Research and Consultancy

--- client/src/Screens/ProfileScreen/ProfileScreen.jsx
+++ client/src/Screens/ProfileScreen/ProfileScreen.jsx
@@ -29,88 +29,54 @@ const ProfileScreen = () => {
         <div className=" p-3 w-full md:w-3/4  flex flex-col justify-center items-center  rounded-xl bg-white ">
           <div className="text-gray-400 text-2xl">
             <div className="grid md:grid-cols-2 text-sm">
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold text-black">NAME: </div>
-                <div className="px-4 py-2">Jane Doe</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold text-black">EMAIL:</div>
-                <div className="px-4 py-2">janedoe@gmail.com</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold text-black">BIO:</div>
-                <div className="px-4 py-2 h-6 overflow-hidden">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Commodi assumenda neque tempore fugiat fugit sunt autem earum,
-                  esse ad sed dolorum doloremque, aliquam perferendis dolore
-                  explicabo eveniet quasi labore dignissimos ipsa error alias
-                  maiores eius totam magni. Omnis, maxime reiciendis. Qui labore
-                  est nostrum cupiditate eveniet maiores dignissimos sint ipsum
-                  ipsa. Esse accusantium voluptas, vero laboriosam consequuntur
-                  consequatur eveniet corrupti, facilis dolorem voluptate
-                  reprehenderit recusandae vel magni cum veritatis culpa, sequi
-                  incidunt asperiores. Possimus animi repellat pariatur ullam
-                  aut dicta minima consequatur. Commodi autem consequuntur error
-                  pariatur, vitae, odio assumenda quisquam ipsum dolorem
-                  accusamus labore qui consequatur. Nesciunt mollitia vero atque
-                  iure ex dolore, sit ipsa vitae temporibus optio consectetur
-                  blanditiis, numquam laborum officiis nemo eos facere debitis
-                  nam id quos eaque? Aliquam aliquid praesentium deleniti quo ab
-                  ea harum ex laborum aspernatur voluptatibus ratione nesciunt,
-                  inventore quibusdam eligendi ipsam placeat animi. Obcaecati
-                  nemo necessitatibus ipsa possimus eius optio rerum fugit
-                  recusandae illum voluptate, exercitationem libero. Voluptatem
-                  magnam id est sunt, eaque voluptatibus reiciendis quos
-                  inventore nam assumenda ut sit eum. Accusamus deserunt ad
-                  aliquid iure tempora voluptate delectus dolore asperiores
-                  ipsum itaque rerum laboriosam autem a, accusantium, suscipit
-                  doloremque in aspernatur, nulla fugiat perspiciatis. Possimus
-                  cupiditate provident mollitia ad, voluptas rem dolorem
-                  corporis numquam, eaque ab assumenda quis quibusdam facere
-                  similique, voluptatibus est repellat eveniet eos sunt
-                  officiis. Reiciendis incidunt fugiat fugit repudiandae
-                  impedit. Ea, possimus esse! Unde aliquam quos quod praesentium
-                  eum illum eos delectus tenetur possimus exercitationem earum
-                  alias, quidem, adipisci quo placeat laudantium voluptate
-                  dignissimos molestias.
-                </div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold text-black">
-                  REGISTERED AT:
-                </div>
-                <div className="px-4 py-2">11 Aug 2024</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold text-black">
-                  BLOGGER STATUS:{" "}
-                </div>
-                <div className="mx-4 my-2 rounded-2xl text-black bg-green-400 flex flex-col justify-center items-center">
-                  Approved
-                </div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold text-black">
-                  EMPLOYEE STATUS:{" "}
-                </div>
-                <div className="mx-4 my-2  rounded-2xl text-black bg-red-400 flex flex-col justify-center items-center">
-                  Rejected
-                </div>
-              </div>
-
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold text-black">
-                  DATE EMPLOYED:
-                </div>
-                <div className="px-4 py-2">11-Aug-2024</div>
-              </div>
-
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold text-black">
-                  DATE RETRENCHED:
-                </div>
-                <div className="px-4 py-2">11-Aug-2024</div>
-              </div>
+              {[
+                ["NAME", "Jane Doe", "", "", ""],
+                ["EMAIL", "janedoe@gmail.com", "", "", ""],
+                [
+                  "BIO",
+                  "Our pragmatic toolkit is designed to unleash the full potential of administrative support personnel, empowering them to execute their work excellently and efficiently. Through our coaching and training programs, we elevate 21st-century essential skills including organization, foresight, and proactiveness, ensuring assistants stay two steps ahead and always on the ball. Our coaching goes beyond the conventional. We harness the power of tested AI hacks and tech tools, incorporating innovative and strategic methods to streamline workflow and boost productivity.",
+                  "",
+                  "h-8 overflow-y-hidden",
+                  "",
+                ],
+                ["REGISTERED AT", "11-Aug-2024", "", "", ""],
+                [
+                  "BLOGGER STATUS",
+                  "Approved",
+                  "",
+                  "bg-green-400 text-slate-800 h-8 flex flex-col justify-center items-center m-2 rounded-3xl",
+                  "",
+                ],
+                [
+                  "EMPLOYEE STATUS",
+                  "Rejected",
+                  "",
+                  "bg-red-400 text-slate-800 h-8 flex flex-col justify-center items-center m-2 rounded-3xl",
+                  "",
+                ],
+                ["DATE EMPLOYED", "21-May-2024", "", "", ""],
+                ["DATE RETRENCHED", "08-Aug-2024", "", "", ""],
+              ].map(
+                ([
+                  dataLabel,
+                  dataValue,
+                  labelClass,
+                  valueClass,
+                  containerClass,
+                ]) => (
+                  <div
+                    key={dataLabel}
+                    className={`grid grid-cols-2 ${containerClass}`}
+                  >
+                    <div
+                      className={`px-4 py-2 font-semibold text-black ${labelClass}`}
+                    >
+                      {dataLabel}:
+                    </div>
+                    <div className={`px-4 py-2 ${valueClass}`}>{dataValue}</div>
+                  </div>
+                )
+              )}
             </div>
           </div>
           <button className="button text-2xl font-extrabold rounded-xl w-full block  p-3 my-4">
