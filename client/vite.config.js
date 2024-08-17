0eeb9f0337fce349983018b3fdaca5fa61c0f498
oluoko <brianotieno586@gmail.com>
2024-08-17 08:46:35 +0300

    Emesa Research and Consultancy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import nodePolyfills from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      // To include Buffer polyfill
+      protocolImports: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      // Add polyfills for Buffer and other Node.js globals
+      buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
+    },
+  },
 });
