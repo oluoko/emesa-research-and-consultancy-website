b2f76e5e06167e70c941de9e4fd741b6786641d4
oluoko <brianotieno586@gmail.com>
2024-08-17 20:16:32 +0300

    Emesa Research and Consultancy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,51 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import nodePolyfills from "vite-plugin-node-polyfills";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      // To include Buffer polyfill
-      protocolImports: true,
-    }),
-  ],
+  plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
   resolve: {
     alias: {
-      // Add polyfills for Buffer and other Node.js globals
-      buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
+      // Polyfill for Buffer
+      buffer: "buffer",
     },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        // Inject Buffer globally for browser compatibility
+        {
+          name: "provide-buffer",
+          transform(code, id) {
+            if (id.includes("google-auth-library")) {
+              return {
+                code: code.replace(/buffer\.Buffer/g, "globalThis.Buffer"),
+                map: null,
+              };
+            }
+            return null;
+          },
+        },
+      ],
+    },
+  },
+  define: {
+    global: {},
   },
 });
