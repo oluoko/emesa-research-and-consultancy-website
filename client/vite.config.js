035782fcab24af42e52fea82fe340c444c98e117
oluoko <brianotieno586@gmail.com>
2024-08-17 20:22:12 +0300

    Emesa Research and Consultancy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,51 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: "globalThis",
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
-  },
-  resolve: {
-    alias: {
-      // Polyfill for Buffer
-      buffer: "buffer",
-    },
-  },
-  build: {
-    rollupOptions: {
-      plugins: [
-        // Inject Buffer globally for browser compatibility
-        {
-          name: "provide-buffer",
-          transform(code, id) {
-            if (id.includes("google-auth-library")) {
-              return {
-                code: code.replace(/buffer\.Buffer/g, "globalThis.Buffer"),
-                map: null,
-              };
-            }
-            return null;
-          },
-        },
-      ],
-    },
-  },
-  define: {
-    global: {},
-  },
 });
