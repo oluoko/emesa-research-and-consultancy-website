87aa758ba04c836b29a34684078fecc5650a9dbc
oluoko <brianotieno586@gmail.com>
2024-08-19 12:21:41 +0300

    Emesa Research and Consultancy

---
+++ client/src/Components/Ads.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Ads = () => {
+  return (
+    <div
+      id="ads"
+      className="absolute top-0 bottom-8 right-4 w-1/6 text-white bg-black rounded-xl"
+    >
+      Ads
+    </div>
+  );
+};
+
+export default Ads;
--- client/src/Components/Ads/Ads.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Ads.css";
-
-const Ads = () => {
-  return <div id="ads">Ads</div>;
-};
-
-export default Ads;
