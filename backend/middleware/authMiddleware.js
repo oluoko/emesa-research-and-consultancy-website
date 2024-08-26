93e639265ccd2df40edcf016a3802d0ba8514e72
oluoko <brianotieno586@gmail.com>
2024-08-27 00:44:40 +0300

    Emesa Research and Consultancy: updated authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -6,14 +6,24 @@ const User = require("../models/userModel.js");
 const protect = asyncHandler(async (req, res, next) => {
   let token;
 
-  // Read JWT from the 'jwt' cookie
-  token = req.cookies.jwt;
+  // Read JWT from the 'jwt' cookie or Authorization header
+  token = req.cookies.jwt || req.headers.authorization?.split(" ")[1];
 
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
       req.user = await User.findById(decoded.userId).select("-password");
+
+      if (!req.user) {
+        res.status(401);
+        throw new Error("Not authorized, user not found");
+      }
+
+      // Check if email is verified
+      if (!req.user.isVerified) {
+        res.status(401);
+        throw new Error("Not authorized, email not verified");
+      }
 
       next();
     } catch (error) {
@@ -32,25 +42,27 @@ const admin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
     next();
   } else {
-    res.status(401);
+    res.status(403); // 403 Forbidden for authorized user without sufficient permissions
     throw new Error("Not authorized as an admin");
   }
 };
 
+// User must be a blogger
 const blogger = (req, res, next) => {
   if (req.user && req.user.isBlogger) {
     next();
   } else {
-    res.status(401);
+    res.status(403);
     throw new Error("Not authorized as a blogger");
   }
 };
 
+// User must be an employee
 const employee = (req, res, next) => {
   if (req.user && req.user.isEmployee) {
     next();
   } else {
-    res.status(401);
+    res.status(403);
     throw new Error("Not authorized as an employee");
   }
 };
