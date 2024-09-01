2c24d05464eb262d2ac6eb3ce4b3c04f60414a22
oluoko <brianotieno586@gmail.com>
2024-09-01 11:00:05 +0300

    Emesa Research and Consultancy: Updating AdminRoute.jsx

--- client/src/Components/AdminRoute.jsx
+++ client/src/Components/AdminRoute.jsx
@@ -1,12 +1,15 @@
-// import { Navigate, Outlet } from "react-router-dom";
-// import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
-// const AdminRoute = () => {
-//   const { userInfo } = useSelector((state) => state.auth);
-//   return userInfo && userInfo.isAdmin ? (
-//     <Outlet />
-//   ) : (
-//     <Navigate to="/login" replace />
-//   );
-// };
-// export default AdminRoute;
+const AdminRoute = () => {
+  const { userInfo } = useContext(AuthContext);
+
+  return userInfo && userInfo.isAdmin ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" replace />
+  );
+};
+
+export default AdminRoute;
