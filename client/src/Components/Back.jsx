aeb629425b7507fa374e369f109aace063d30544
oluoko <brianotieno586@gmail.com>
2024-09-02 21:49:18 +0300

    Emesa Research and Consultancy: Updated the component Back.jsx

--- client/src/Components/Back.jsx
+++ client/src/Components/Back.jsx
@@ -4,13 +4,28 @@ import { useNavigate, useLocation } from "react-router-dom";
 const Back = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const userData = localStorage.getItem("userData");
 
   useEffect(() => {
     localStorage.setItem("lastPage", location.pathname);
   }, [location]);
+
+  const handleBack = () => {
+    // Check if the lastPage is not login or register and user is logged in
+    if (
+      userData &&
+      location.pathname !== "/login" &&
+      location.pathname !== "/register"
+    ) {
+      navigate(-1);
+    } else {
+      navigate("/"); // Redirect to home if user is not logged in
+    }
+  };
+
   return (
     <button
-      onClick={() => navigate(-1)}
+      onClick={handleBack}
       className="button fixed top-2 left-2 font-bold py-2 px-4 rounded text-xl focus:outline-none focus:shadow-outline"
     >
       Back
