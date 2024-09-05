5efb4d1f233784c39f06818cd0467b06f9c9c3a4
oluoko <brianotieno586@gmail.com>
2024-09-05 11:23:17 +0300

    Emesa Research and Consultancy

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,25 +1,23 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
-  // Create the transporter using environment variables for configuration
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST, // Use EMAIL_HOST from the environment variables
-    port: process.env.EMAIL_PORT, // Use EMAIL_PORT from the environment variables
-    secure: false, // Set to 'false' since port 587 uses STARTTLS
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: false, // 'false' for port 587, which uses STARTTLS
     auth: {
-      user: process.env.EMAIL, // Use EMAIL from the environment variables
-      pass: process.env.EMAIL_PASSWORD, // Use EMAIL_PASSWORD from the environment variables
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
 
   const mailOptions = {
-    from: `"Emesa Research" <${process.env.EMAIL}>`, // Sender's name and email
-    to: options.email, // Recipient email address
-    subject: options.subject, // Email subject
-    text: options.message, // Email message (plaintext)
+    from: `"Emesa Research" <${process.env.EMAIL}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
   };
 
-  // Send the email
   await transporter.sendMail(mailOptions);
 };
 
