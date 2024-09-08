5bed7ec37708ee07283aa3343db592a94938f112
oluoko <brianotieno586@gmail.com>
2024-09-08 12:29:06 +0300

    Updated Server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const colors = require("colors");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const connectDB = require("./config/db.js");
@@ -21,3 +22,13 @@ app.use(cors());
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", googleAuthRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(
+    `\nServer is running in ${process.env.NODE_ENV} mode on port ${port}\n`.blue
+      .bold.underline.inverse
+  );
+});
