39e711ecf35b15e93ea5ca0db259c4745cc9e293
oluoko <brianotieno586@gmail.com>
2024-09-08 11:49:14 +0300

    Emesa Research and Consultancy: Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
-const cors = require("cors"); // Add this
-dotenv.config();
+const cors = require("cors");
 const connectDB = require("./config/db.js");
 const userRoutes = require("./routes/userRoutes.js");
+const googleAuthRoutes = require("./routes/googleAuthRoutes.js");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware.js");
-const { OAuth2Cient } = require("google-auth-library");
-const router = express.Router();
 
+dotenv.config();
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -18,73 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-// Enable CORS for frontend
-app.use(
-  cors({
-    origin: "*", // Allow requests from your frontend
-    credentials: true, // Enable credentials if you're using cookies or authorization headers
-  })
-);
-
-/* GET users listing. */
-router.post("/", async function (req, res, next) {
-  res.header("Access-Controll-Allow-Origin", "http:localhost:5173");
-  res.header("Referrer-Policy", "no-referrer-when-downgrade");
-
-  const redirectURL = "http://127.0.0.1.3000/oauth";
-
-  const oAuth2Client = new OAuth2Cient(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    redirectURL
-  );
-
-  const authorizeUrl = oAuth2Client.generateAuthUrl({
-    access_type: "offline",
-    scope: "https://www.googleapis.com/auth/userinfo.profile openid",
-    prompt: "consent",
-  });
-
-  res.json({ url: authorizeUrl });
-});
-
-async function getUserData(access_token) {
-  const response = await fetch(
-    `https://www.googleapis.com/oauth2/v3/userinfo?access_token${access_token}`
-  );
-  const data = await response.json();
-  console.log("data", data);
-}
-
-/* GET home page.*/
-router.get("/", async function (req, res, nest) {
-  const code = req.query.code;
-  try {
-    const redirectURL = "http://127.0.0.1.3000/oauth";
-
-    const oAuth2Client = new OAuth2Cient(
-      process.env.CLIENT_ID,
-      process.env.CLIENT_SECRET,
-      redirectURL
-    );
-    const res = await oAuth2Client.getToken(code);
-    await oAuth2Client.setCredentials(res.tokens);
-    console.log("Tokens acquired");
-
-    const user = oAuth2Client.credentials;
-    console.log("User's Credentials:", user);
-    await getUserData(user.access_token);
-  } catch (error) {
-    console.log("Error with signing in with Google:", error);
-  }
-});
+app.use(cors());
 
 app.use("/api/users", userRoutes);
-
-app.use(notFound);
-app.use(errorHandler);
-
-app.listen(port, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
-});
+app.use("/api/auth", googleAuthRoutes);
