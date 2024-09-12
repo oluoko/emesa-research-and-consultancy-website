08decb85a36beb723870b983bdaca73b3f0d38c5
oluoko <brianotieno586@gmail.com>
2024-09-12 17:18:30 +0300

    Emesa Research and Consultancy

--- client/src/Screens/AttacheeApplicationScreen.jsx
+++ client/src/Screens/AttacheeApplicationScreen.jsx
@@ -1,17 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
 import Back from "../Components/Back";
+import { useRef } from "react";
+import Toast, { showToast } from "../Components/Toast/Toast";
+import emailjs from "@emailjs/browser";
+import {
+  CONTACTS_SERVICES_TEMPLATE_ID,
+  PUBLIC_KEY,
+  SERVICE_ID,
+} from "../config";
+// npm install --save @emailjs/browser --legacy-peer-deps
 
 const AttacheeApplicationScreen = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(SERVICE_ID, CONTACTS_SERVICES_TEMPLATE_ID, form.current, {
+        publicKey: PUBLIC_KEY,
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          showToast("Message Sent Successfully", "success");
+          form.current.reset();
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+          showToast(error.text, "error");
+        }
+      );
+  };
   return (
     <div className=" relative flex flex-col  vh-100  items-center justify-center m-2">
       <Back />
-
+      <Toast />
       <h2 className="mt-14 md:mt-6 text-2xl font-extrabold text-orange-500">
         APPLY FOR ATTACHMENT
       </h2>
       <div className="contacts-form form w-full md:w-2/3 my-1 md:my-5 p-4 md:p-10 border rounded-lg shadow-lg bg-gray-800 flex flex-col   text-white ">
-        <form className=" flex flex-col items-center  ">
+        <form
+          className=" flex flex-col items-center  "
+          ref={form}
+          onSubmit={sendEmail}
+        >
           <div className="grid md:flex grid-col justify-center md:justify-between items-center w-full my-1 md:my-2">
             <label htmlFor="name" className="text-sm md:text-lg">
               FULL NAME
@@ -21,6 +53,7 @@ const AttacheeApplicationScreen = () => {
               id="name"
               className="col-md-9 pe-5 shadow appearance-none border rounded font-bold m-2 md:m-4 mb-2 md:mb-6 py-1 md:py-4 px-1 md:px-3 text-gray-600 w-full md:w-1/2 bg-inherit leading-tight focus:outline-none focus:shadow-outline focus:border-orange-500"
               placeholder="John Doe"
+              name="name"
             />
           </div>
 
@@ -35,6 +68,7 @@ const AttacheeApplicationScreen = () => {
               id="email"
               className="col-md-9 pe-5 shadow appearance-none border rounded font-bold m-2 md:m-4 mb-2 md:mb-6 py-1 md:py-4 px-1 md:px-3 text-gray-600 bg-inherit leading-tight focus:outline-none focus:shadow-outline focus:border-orange-500  w-full md:w-1/2"
               placeholder="j.doe@company.com"
+              name="email"
             />
           </div>
 
@@ -49,6 +83,7 @@ const AttacheeApplicationScreen = () => {
               id="message"
               className="col-md-9 pe-5 shadow appearance-none border rounded font-bold m-2 md:m-4 mb-2 md:mb-6 py-1 md:py-4 px-1 md:px-3 text-gray-600 bg-inherit leading-tight focus:outline-none focus:shadow-outline focus:border-orange-500  w-full md:w-1/2 overflow-y-hidden                                                         "
               placeholder="Message to the employee telling us about yourself..."
+              name="about"
             />
           </div>
 
@@ -63,6 +98,7 @@ const AttacheeApplicationScreen = () => {
                 className="col-md-9 shadow appearance-none border rounded font-bold m-4 text-gray-600 bg-inherit leading-tight focus:outline-none focus:shadow-outline focus:border-orange-500 w-5/6 md:w-full "
                 id="formFileLg"
                 type="file"
+                name="resume"
               />
               <div className="small text-gray-400 mt-2 w-full">
                 Upload your CV/Resume or any other relevant file. Max file size
