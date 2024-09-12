403ab81eca4a24ff7cf893593b8cc70b21dbbcd0
oluoko <brianotieno586@gmail.com>
2024-09-12 14:13:21 +0300

    Emesa Research and Consultancy: updated Toast.jsx file

--- client/src/Components/Toast/Toast.jsx
+++ client/src/Components/Toast/Toast.jsx
@@ -1,5 +1,3 @@
-// src/components/Toast/Toast.js
-import React from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./Toast.css";
