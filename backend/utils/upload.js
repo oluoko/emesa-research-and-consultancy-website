d8b10ef0b632442298b5f2ea2b42ce94aa253cbf
oluoko <brianotieno586@gmail.com>
2024-09-12 12:03:46 +0300

    Emesa Research and Consultancy: updated upload.js

--- backend/utils/upload.js
+++ backend/utils/upload.js
@@ -1,11 +1,33 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+// Check if the directory exists, and if not, create it
+const ensureDirectoryExists = (dir) => {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+};
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "uploads/");
+    // Get the folder from request or set a default one
+    const folder = req.body.folder || "userProfilePics"; // Example: User sends folder choice in request body
+
+    // Define the base uploads path
+    const basePath = "uploads/";
+
+    // Create the full path to the desired folder
+    const fullPath = path.join(basePath, folder);
+
+    // Ensure the folder exists
+    ensureDirectoryExists(fullPath);
+
+    // Use the chosen folder as the destination
+    cb(null, fullPath);
   },
   filename(req, file, cb) {
+    // Generate the filename
     cb(
       null,
       `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
