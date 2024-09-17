d8d8429c28c82f0d98ad8097a520849632325d02
oluoko <brianotieno586@gmail.com>
2024-09-17 15:37:00 +0300

    Emesa Research and Consultancy

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -12,46 +12,11 @@ const {
 } = require("../controllers/userController.js");
 const { protect, admin } = require("../middleware/authMiddleware.js");
 
-const multer = require("multer");
-const path = require("path");
-
-const router = express.Router();
-
-// Set up multer for profile image uploads
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, "uploads/userProfilePics/");
-  },
-  filename(req, file, cb) {
-    cb(null, `${req.user._id}${path.extname(file.originalname)}`);
-  },
-});
-
-// Check file type
-function checkFileType(file, cb) {
-  const filetypes = /jpg|jpeg|png/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
-
-  if (extname && mimetype) {
-    return cb(null, true);
-  } else {
-    cb("Images only!");
-  }
-}
-
-const upload = multer({
-  storage,
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb);
-  },
-});
-
 // Profile routes
 router
   .route("/profile")
   .get(protect, getUserProfile)
-  .put(protect, upload.single("profilePic"), updateUserProfile);
+  .put(protect, updateUserProfile);
 
 router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/auth", authUser);
